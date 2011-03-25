288cb8157e7ebdec081d46c8018d112ef5a3c464
Tanzeeb Khalili <tanzeeb@gmail.com>
2011-03-25 00:49:27 -0400

    latest database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110325043417) do
+ActiveRecord::Schema.define(:version => 20110325044006) do
+
+  create_table "followerships", :force => true do |t|
+    t.integer  "follower_id"
+    t.integer  "following_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "tweets", :force => true do |t|
     t.integer  "user_id"
