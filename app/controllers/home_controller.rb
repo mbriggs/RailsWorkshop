a6ac1500e76ef89dbe4edec136cda42b72721f4a
Tanzeeb Khalili <tanzeeb@gmail.com>
2011-03-26 14:09:04 -0400

    more stubs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,14 +6,15 @@ class HomeController < ApplicationController
   end
 
   def login
+    # FIXME get the user!
     session[:user] = User.find_by_username_and_password \
-      params[:username], params[:password] 
+      params[:password], params[:username] 
 
     redirect_to root_path
   end
 
   def logout
-    session[:user] = nil
+    # FIXME drop the session
     redirect_to root_path
   end
 end
